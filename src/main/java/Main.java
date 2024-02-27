cedaf8db83e00b0176352ffb95b29b3a40f7638c
Zachary Beach <zacharybeach@Zacharys-MacBook-Pro-92.local>
2024-02-26 18:40:45 -0700

    Pushing P

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,41 @@
-public class Main {
-    ///pushing p part two
-    public static void main(String[] args) {
-        System.out.println("Testing Dockerfile");
-    }
+import java.util.Arrays;
 
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        if (args.length < 2) {
+
+            System.out.println("Invalid input. Please provide a command and input numbers.");
+            return;
+        }
+
+        String command = args[0];
+        int[] numbers = Arrays.stream(args).skip(1).mapToInt(Integer::parseInt).toArray();
+
+        String result = "";
+        switch (command) {
+            case "add":
+                System.out.println("Output: " + calculator.add(numbers[0], numbers[1]));
+                break;
+            case "subtract":
+                System.out.println("Output: " + calculator.subtract(numbers[0], numbers[1]));
+                break;
+            case "multiply":
+                System.out.println("Output: " + calculator.multiply(numbers[0], numbers[1]));
+                break;
+            case "divide":
+                System.out.println("Output: " + calculator.divide(numbers[0], numbers[1]));
+                break;
+            case "fibonacciNumberFinder":
+                System.out.println("Output: " + calculator.fibonacciNumberFinder(numbers[0]));
+                break;
+            case "intToBinaryNumber":
+                System.out.println("Output: " + calculator.intToBinaryNumber(numbers[0]));
+                break;
+            default:
+                System.out.println("Invalid command.");
+                return;
+        }
+
+    }
 }
